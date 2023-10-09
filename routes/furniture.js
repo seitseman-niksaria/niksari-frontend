95521ae344d721462d32d22ca70a567c93607e7c
svidzger <german.svidzinskij@gmail.com>
2023-10-09 14:03:32 +0300

    simple fetch request

--- routes/furniture.js
+++ routes/furniture.js
@@ -1,11 +1,37 @@
-import { View, Text } from 'react-native';
+import { useState, useEffect } from 'react';
+import { View, Text, FlatList } from 'react-native';
 
-export default function Furniture(){
-    return(
-        <View>
-        <Text>
-             {'Here you can see furnitures!'}
-        </Text>
-        </View>
-      )
-};
+export default function Furniture() {
+  const [data, setData] = useState([]);
+  const [noFurniture, setNoFurniture] = useState('');
+
+  useEffect(() => fetchData(), []);
+
+  const fetchData = () => {
+    fetch('http://127.0.0.1:8000/furniture-models/')
+      .then((response) => response.json())
+      .then((responseData) => {
+        setData(responseData);
+        if (data === null) {
+          setNoFurniture('No furniture avaible.')
+        }
+        console.log(responseData);
+      })
+      .catch((err) => console.error(err));
+  };
+
+  return (
+    <View>
+      <FlatList
+        data={data}
+        ListEmptyComponent={() => <Text>{noFurniture}</Text>}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item.furniture_name}</Text>
+          </View>
+        )}
+      ></FlatList>
+    </View>
+  );
+}
