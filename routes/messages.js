d0c326a61655a85b3410c5837c772904cdc9abfe
svidzger <german.svidzinskij@gmail.com>
2023-11-12 22:11:39 +0200

    added instructions logic, reusable functions and comments

--- routes/messages.js
+++ routes/messages.js
@@ -1,112 +1,157 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import { GiftedChat } from 'react-native-gifted-chat';
 import uuid from 'react-native-uuid';
 import { fetchInstructions, fetchModels } from '../components/api';
 
-
 export default function Messages(props) {
-
   const [messages, setMessages] = useState([]);
-  const [modelNameExpected, setModelNameExpected] = useState(false);
-  const [furnitureModels, setFurnitureModels] = useState([]);
+  const [descriptionExpected, setDescriptionExpected] = useState(false);
+  const [instructionExpected, setInstructionExpected] = useState(false);
+  const [models, setModels] = useState([]);
   const [instructions, setInstructions] = useState([]);
-  const [missingData, setMissingData] = useState('');
 
   useEffect(() => {
-    models();
-    fetchingInstructions();
+    // Set all furniture models in to a models useState.
+    // HTTP GET request is located in components/api.js.
+    fetchModels().then((resp) => {
+      setModels(resp);
+    });
+    // Set all instructions in to an instructions useState.
+    // HTTP GET request is located in components/api.js.
+    fetchInstructions().then((resp) => {
+      setInstructions(resp);
+    });
+    // Set initial bot message on page load.
     setMessages([props.initialMessage]);
   }, []);
 
-
-  // fetch all available models
-  const models = async () => {
-    try{
-      const data = await fetchModels();
-      setFurnitureModels(data);
-      if (data === null) {
-        setMissingData('No furniture models available.');
-      }
-    } catch (error) {
-      console.error('Error fetching models:', error);
-      setMissingData('Error fetching furniture models.');
+  // Reusable bot responce function
+  const botResponse = (text) => {
+    const responce = {
+      _id: uuid.v4(),
+      text: text,
+      createdAt: new Date(),
+      user: {
+        _id: 'chatbot',
+        name: 'Chatbot',
+      },
     };
+    return responce;
   };
 
-  // fetch all available instructions
-  const fetchingInstructions = async () => {
-    try {
-      const data = await fetchInstructions();
-      for (let i=0; i<data.length; i++){
-        if (data[i].instruction_name=="General"){
-          setInstructions(data[i].instruction_text);
-          break;
-        }
+  // Function to get instruction by name
+  const getInstruction = (iName) => {
+    const instruction = instructions.map((i) => {
+      if (i.instruction_name === iName) {
+        return i.instruction_text;
       }
-      console.log(instructions)
-      if (data === null) {
-        setMissingData('No instructions available.');
-      }
-    } catch (error) {
-      console.error('Error fetching instructions:', error);
-      setMissingData('Error fetching instructions.');
-    };
+    });
+    return instruction;
   };
 
-
-  // function that handles user input sends
+  // Function that handles user input sends
   const onSend = (messages = []) => {
     const msg = messages[0];
     setMessages((previousMessages) =>
       GiftedChat.append(previousMessages, messages)
     );
-    if (modelNameExpected) {
-      furnitureInfo(msg.text);
-      setModelNameExpected(false);
-    };
+    // If user is looking for description of a model
+    if (descriptionExpected) {
+      modelDescription(msg.text);
+      setDescriptionExpected(false);
+      // If user is looking for care instructions of a model
+    } else if (instructionExpected) {
+      modelInstruction(msg.text);
+      setInstructionExpected(false);
+    }
   };
 
-  // chatbot returns description of a furniture model by user input
-  const furnitureInfo = (userInput) => {
-    const model = furnitureModels.find(
-      (furniture) => furniture.furniture_name === userInput.toUpperCase()
+  // Function that returns description of a furniture model
+  // by model name provided by user
+  const modelDescription = (userInput) => {
+    const model = models.find(
+      (m) => m.furniture_name === userInput.toUpperCase()
     );
-
-    //if a furniture is found by the name user gave
+    // If the furniture model is found with the name given by the user,
+    // the description of the model is saved in the messages useState.
     if (model) {
-      const botResponse = {
-        _id: uuid.v4(),
-        text:
-          `Description of ${model.furniture_name} model: \n\n${model.furniture_description}`,
-        createdAt: new Date(),
-        user: {
-          _id: 'chatbot',
-          name: 'Chatbot',
-        },
-      };
       setMessages((previousMessages) =>
-        GiftedChat.append(previousMessages, botResponse)
+        GiftedChat.append(
+          previousMessages,
+          botResponse(
+            `Description of ${model.furniture_name} model: \n\n${model.furniture_description}`
+          )
+        )
       );
-      // if no furniture by that name is found
+      // If model is not found then negative responce is saved in messages useState.
     } else {
-      const botResponse = {
-        _id: uuid.v4(),
-        text: 'No info available',
-        createdAt: new Date(),
-        user: {
-          _id: 'chatbot',
-          name: 'Chatbot',
-        },
-      };
       setMessages((previousMessages) =>
-        GiftedChat.append(previousMessages, botResponse)
+        GiftedChat.append(
+          previousMessages,
+          botResponse(`There is no model named ${userInput}. Try again!`)
+        )
       );
     }
-    // gives the options again, with a different greeting, id and time (doesn't matter if info is found or not)
+    // Returns the options again, with a different greeting
+    // id and time (doesn't matter if info is found or not).
     setMessages((previousMessages) =>
       GiftedChat.append(previousMessages, props.initialMessage2)
     );
+  };
 
+  // Function that returns care instructions of a furniture model.
+  // Instructions depends if model is outdoor or have leather materials in it.
+  // This function lacks of logic for if model is outdoor and
+  // leather at the same time.
+  // Note! This function should be improved during the next sprint.
+  const modelInstruction = (userInput) => {
+    const model = models.find(
+      (m) => m.furniture_name === userInput.toUpperCase()
+    );
+    // If model name is spelled wrong or model does not exist
+    if (!model) {
+      setMessages((previousMessages) =>
+        GiftedChat.append(
+          previousMessages,
+          botResponse(`There is no model named ${userInput}. Try again!`)
+        )
+      );
+      // If model is outdoor, then this instructions will be sent to a user.
+    } else if (model.outdoor === true) {
+      setMessages((previousMessages) =>
+        GiftedChat.append(
+          previousMessages,
+          botResponse(getInstruction('Outdoor'))
+        )
+      );
+      // If model has leather materials in it, then this instructions is returned.
+    } else if (model.leather === true) {
+      setMessages((previousMessages) =>
+        GiftedChat.append(
+          previousMessages,
+          botResponse(
+            // BUG for some reason can't insert new line
+            `${getInstruction('Elmo leather')}\n\n${getInstruction(
+              'Vegetable tanned leather'
+            )}`
+          )
+        )
+      );
+      // If model lacks any of the above, then user is provided
+      // with general care instructions.
+    } else {
+      setMessages((previousMessages) =>
+        GiftedChat.append(
+          previousMessages,
+          botResponse(getInstruction('General'))
+        )
+      );
+    }
+    // Returns the options again, with a different greeting
+    // id and time (doesn't matter if info is found or not).
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, props.initialMessage2)
+    );
   };
 
   // chosen option turns into user's message and shows up on screen
@@ -137,14 +182,13 @@ export default function Messages(props) {
         // add logic so user can take/choose a picture
         break;
       case 'instructions':
-      if (!instructions){
-        fetchInstructions();
-      }console.log(instructions);
-        botResponseText = `Sure! These are the instructions for all models:\n\n ${instructions}`;
+        botResponseText = 'Sure! Please provide me with a model name.';
+        setInstructionExpected(true);
+        onSend();
         break;
       case 'info':
         botResponseText = 'Sure! Please provide me with a model name.';
-        setModelNameExpected(true);
+        setDescriptionExpected(true);
         onSend();
         break;
       default:
@@ -152,19 +196,10 @@ export default function Messages(props) {
           "I didn't understand your selection. Please try again.";
         break;
     }
-    // chatbot's response
-    const botResponse = {
-      _id: uuid.v4(),
-      text: botResponseText,
-      createdAt: new Date(),
-      user: {
-        _id: 'chatbot',
-        name: 'Chatbot',
-      },
-    };
+
     // update the chat messages with the bot's response
     setMessages((previousMessages) =>
-      GiftedChat.append(previousMessages, botResponse)
+      GiftedChat.append(previousMessages, botResponse(botResponseText))
     );
   };
 
