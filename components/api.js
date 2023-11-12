d23e4e4a3409127c8dc3b77656f14769f4ddec26
svidzger <german.svidzinskij@gmail.com>
2023-11-12 19:47:57 +0200

    indentation fix and comments

--- components/api.js
+++ components/api.js
@@ -1,18 +1,19 @@
+// HTTP GET request for all avaible futniture models
 export const fetchModels = async () => {
-    try {
-        const resp = await fetch('http://127.0.0.1:8000/furniture-models/');
-        return await resp.json();
-    } catch (err) {
-        return console.error(err);
-    }
-  };
-  
-  export const fetchInstructions = async () => {
-    try {
-          const resp = await fetch('http://127.0.0.1:8000/instructions/');
-          return await resp.json();
-      } catch (err) {
-          return console.error(err);
-      }
-  };
-  
+  try {
+    const resp = await fetch('http://127.0.0.1:8000/furniture-models/');
+    return await resp.json();
+  } catch (err) {
+    return console.error(err);
+  }
+};
+
+// HTTP GET request for all avaible instructions
+export const fetchInstructions = async () => {
+  try {
+    const resp = await fetch('http://127.0.0.1:8000/instructions/');
+    return await resp.json();
+  } catch (err) {
+    return console.error(err);
+  }
+};
