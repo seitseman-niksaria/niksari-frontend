db3cf13207d6e54fdfa94620a71ad37f3f39985e
Ada Meriläinen <ada.merilainen@gmail.com>
2023-11-13 14:52:54 +0200

    Deleted home.js and fixed navigation

--- App.js
+++ App.js
@@ -1,7 +1,4 @@
 import { NavigationContainer } from "@react-navigation/native";
-import Home from "./routes/home";
-import Furniture from "./routes/furniture";
-import Instructions from "./routes/instructions";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import Chatbot from "./routes/chatbot";
 import NiksariCamera from "./routes/NiksariCamera";
@@ -13,9 +10,6 @@ export default function App() {
   return (
     <NavigationContainer>
       <Drawer.Navigator initialRouteName="Chatbot">
-        <Drawer.Screen name="Home" component={Home} />
-        <Drawer.Screen name="Furniture" component={Furniture} />
-        <Drawer.Screen name="Instructions" component={Instructions} />
         <Drawer.Screen name="Camera" component={NiksariCamera} />
         <Drawer.Screen name="Chatbot" component={Chatbot} />
       </Drawer.Navigator>
